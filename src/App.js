f55f97f4b71b3f6dbde6e1cf9202014edebf1abf
btilling <ben@savingunlocked.com>
2020-07-19 06:38:09 +0100

    Fix packages amd add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './components/Navigation';
+import TransactionView from './components/TransactionView';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/TransactionView.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TransactionView = () => <div>Transaction View</div>;
+
+export default TransactionView;
