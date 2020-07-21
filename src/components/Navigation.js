869e7e3e53fa601d84f8cef301bcc853fb00b708
btilling <ben@savingunlocked.com>
2020-07-21 09:10:43 +0100

    Change navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { Segment, Menu, Sidebar } from 'semantic-ui-react';
-import TransactionView from './TransactionView';
+import { Menu } from 'semantic-ui-react';
 
 const Navigation = () => {
   return (
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar as={Menu} visible animation="push" inverted width="thin">
-        <Menu.Item as="a">Transactions</Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher>
-        <Segment basic>
-          <TransactionView />
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+    <Menu secondary>
+      <Menu.Item as="a" active>
+        Transactions
+      </Menu.Item>
+    </Menu>
   );
 };
 
